367d6745b4bbd71fa70680ce875aaac7653f6cb5
Sanjay-2004 <sanjay.agamamidi@gmail.com>
2023-06-13 15:27:00 +0530

    Reporter reading the requests from users

--- Backend/index.js
+++ Backend/index.js
@@ -2,31 +2,28 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
-// Import the bugs route file
 import bugsRouter from './Routes/Bugs.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
 mongoose
   .connect('mongodb+srv://Sanjay:Sanjay.04@blcpmgmt.scewiep.mongodb.net/Project', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connected to BLCPMGMT database');
+    console.log('Connected to MongoDB');
   })
   .catch(err => {
     console.error('Error connecting to MongoDB:', err);
   });
 
-// Register the bugs route
 app.use('/create-bug', bugsRouter);
+app.use('/bug-reports', bugsRouter); 
 
-// Start the server
 const port = 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
