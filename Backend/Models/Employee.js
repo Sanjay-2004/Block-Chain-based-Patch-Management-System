b89641fe220e8072a8e76b0f7814c1cd214427cd
Sanjay-2004 <sanjay.agamamidi@gmail.com>
2023-06-28 14:51:19 +0530

    NodeMailer

--- Backend/Models/Employee.js
+++ Backend/Models/Employee.js
@@ -5,6 +5,7 @@ const EmployeeSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
+  verified: { type: Boolean, default: false },
 });
 
 const Employee = mongoose.model("Employees", EmployeeSchema, "Employees");
--- Backend/Routes/Employees.js
+++ Backend/Routes/Employees.js
@@ -1,6 +1,36 @@
 import express from "express";
 const router = express.Router();
+import nodemailer from "nodemailer";
 import Employee from "../Models/Employee.js";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  service: "gmail",
+  port: 587,
+  secure: true,
+  auth: {
+    user: "sj18apr.04@gmail.com",
+    pass: "phxnzzpwbsmvxonp",
+  },
+});
+
+const sendEmail = async (email, subject, text) => {
+  try {
+    const mailing = {
+      from: '"Admin1" <admin@company.com>',
+      to: email,
+      subject: subject,
+      html: `<p>${text}</p>`,
+    };
+
+    transporter.sendMail(mailing, (error, info) => {
+      if (error) console.log("Error sending mail: ", error);
+      else console.log("Email sent: ", info.response);
+    });
+  } catch (error) {
+    console.log("Error sending mail: ", error);
+  }
+};
 
 router.post("/", async (req, res) => {
   try {
@@ -12,6 +42,11 @@ router.post("/", async (req, res) => {
 
     const newEmployee = await Employee.create(req.body);
     if (newEmployee) {
+      const subject = "WELCOME TO OUR COMPANY";
+      const text = `This is your password: ${req.body.password}`;
+
+      sendEmail(req.body.email, subject, text);
+
       console.log("Creation Successful");
       res.sendStatus(200);
     } else {
